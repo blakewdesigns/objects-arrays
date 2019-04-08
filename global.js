026dc48adc52ec9fe45ff48d161ecbffbff23dd8
Gabe Wright <gabe1331@gmail.com>
2019-04-08 15:25:15 -0400

    added steps

--- global.js
+++ global.js
@@ -31,13 +31,27 @@ let myFriends = [
     }  
   ]
 
-// Your code here`
+// 1.) Add a friend to the myFriends array
+// Your code here
+
+// 2.) remove a friend from the array (not the one you just added)
+// Your code here
+
+// 3.) console.log the array in reverse
+// Your code here
+
+// 4.) without modifying the array from about, change the first object's firstName value to Timothy
+// your code here
+
+// 5.) give the last friend in the array a new prop, "nickName" with the value of "Danny". 
+// your code here
 
 myFriends.forEach((friends) => {
     let friend = `
         <div class="col">
             <div class="friend">
                 <h3>${friends.firstName} ${friends.lastName}</h3>
+                <span>${friends.nickName ? "nickname: " + friends.nickName : ""}</span>
                 <p>${friends.age}</p>
             </div>
         </div>
